2886e8ca935d6c59bf083f60653cff9961c39ae7
Albert <albertlukose007@gmail.com>
2025-12-12 13:06:48 +0530

    Fix static file routing

--- maxfit/maxfit/urls.py
+++ maxfit/maxfit/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),  # 👈 main site
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # your existing URL patterns...
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
